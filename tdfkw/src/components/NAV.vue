5b627fcf6e0497a98010172230671fdb5817b695
蓝鹰小旋 <LanYinxianxuan@163.com>
2026-04-11 18:25:05 +0000

    新增导航组件样式，优化首页布局

--- tdfkw/src/components/NAV.vue
+++ tdfkw/src/components/NAV.vue
@@ -4,3 +4,6 @@
 <template>
   <h1>糖豆方块屋</h1>
 </template>
+<style>
+
+</style>
--- tdfkw/src/views/HomeView.vue
+++ tdfkw/src/views/HomeView.vue
@@ -7,26 +7,7 @@
     <h1 class="logo-h1">糖豆方块屋</h1>
   </div>
   <div class="a">
-    <!-- <a @click="router.push('/start')">现在开始</a> -->
      <router-link to="/start">现在开始</router-link>
   </div>
   
 </template>
-<style>
-.logo {
-  background-color: rgb(72, 255, 0);
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.logo-h1 {
-  margin: 0;
-  font-size: 3rem;
-}
-.a {
-  display: flex;
-  justify-content: center;
-}
-</style>
