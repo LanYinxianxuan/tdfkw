3b056b6605b99fcf03f8863a363b3d13a1198fb0
蓝鹰小旋 <LanYinxianxuan@163.com>
2026-05-06 16:15:30 +0000

    修改了注册码页按钮无样式的错误

--- tdfkw/src/views/RegisterView.vue
+++ tdfkw/src/views/RegisterView.vue
@@ -39,7 +39,7 @@ const postRegisterCode = async () => {
         <li>收到注册码后，按照提示完成注册流程</li>
       </ul>
       <input type="password" placeholder="请输入注册码" v-model="registerCode" />
-      <button class="btn" @click="postRegisterCode">现在开始</button>
+      <button class="btn-primary" @click="postRegisterCode">现在开始</button>
     </div>
   </div>
 </template>
